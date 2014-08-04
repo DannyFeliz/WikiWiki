45f988d80352eab04449c361e5db3699507cdba9
starmy <lewdin_acevedo.06@hotmail.com>
2014-08-03 23:46:17 -0400

    Mejorando el registrar y demás defectos.

--- WikiWiki/WikiWiki/Controllers/LogeoController.cs
+++ WikiWiki/WikiWiki/Controllers/LogeoController.cs
@@ -22,13 +22,13 @@ namespace Blog.Controllers
         private UsersContext db = new UsersContext();
         RepositorioUsuario repositorioUsuario = new RepositorioUsuario();
 
-        public ActionResult Index()
+        public ActionResult Iniciar()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Index(Logeo model)
+        public ActionResult Iniciar(Logeo model)
         {
 
             if(ModelState.IsValid){
@@ -47,6 +47,44 @@ namespace Blog.Controllers
             }
             
             return View(model);
+        }
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Registrar(Registro datosUsuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Registros.Add(new Registro
+                {
+                    nombre = datosUsuario.nombre,
+                    apellido = datosUsuario.apellido,
+                    usuario = datosUsuario.usuario,
+                    clave = md5(datosUsuario.clave),
+                    nacimiento = datosUsuario.nacimiento,
+                    hora_de_registro = DateTime.Now,
+                    direccion_ip = Request.ServerVariables["REMOTE_ADDR"],
+                    email = datosUsuario.email,
+                    direccion = datosUsuario.direccion,
+                    ocupacion = datosUsuario.ocupacion
+                });
+                db.SaveChanges();
+                if (repositorioUsuario.validarUsuario(datosUsuario.usuario, md5(datosUsuario.clave)))
+                {
+                    ViewBag.usuario = datosUsuario.usuario;
+                    FormsAuthentication.SetAuthCookie(datosUsuario.usuario, true);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    return Content("No pudimos confirmar el inicio de seccion, intentalo mas tarde.");
+                }
+            }
+            return View();
         }
 
         public ActionResult Salir()
