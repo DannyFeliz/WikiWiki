ed7e765017c2b15d1de0c6c662c761c0b17ac10b
starmy <lewdin_acevedo.06@hotmail.com>
2014-08-05 15:55:33 -0400

    Modificaciones Lewdin

--- WikiWiki/WikiWiki/Controllers/LogeoController.cs
+++ WikiWiki/WikiWiki/Controllers/LogeoController.cs
@@ -74,7 +74,6 @@ namespace Blog.Controllers
                     direccion_ip = Request.ServerVariables["REMOTE_ADDR"],
                     email = datosUsuario.email,
                     direccion = datosUsuario.direccion,
-                    ocupacion = datosUsuario.ocupacion
                 });
                 db.SaveChanges();
                 if (repositorioUsuario.validarUsuario(datosUsuario.usuario, md5(datosUsuario.clave)))
@@ -90,6 +89,47 @@ namespace Blog.Controllers
             }
             return View();
         }
+
+        //public ActionResult CompletarRegistro(int id = 0)
+        //{
+        //    return View(db.Registros.Find(id));
+        //}
+        
+        //[HttpPost]
+        //public ActionResult CompletarRegistro(Registro datosUsuario, string foto ,int id = 0)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        var registro = db.Registros.Find(id);
+        //        registro.ocupacion = datosUsuario.ocupacion;
+        //        //db.SaveChanges();
+                
+        //        var use = db.Userios.Find(registro.registro_id);
+        //        return Content(use.foto, use.usuario1);
+        //        //db.Registros.Add(new Registro
+        //        //{
+        //        //    nombre = datosUsuario.nombre,
+        //        //    apellido = datosUsuario.apellido,
+        //        //    usuario = datosUsuario.usuario,
+        //        //    clave = md5(datosUsuario.clave),
+        //        //    nacimiento = datosUsuario.nacimiento,
+        //        //    hora_de_registro = DateTime.Now,
+        //        //    direccion_ip = Request.ServerVariables["REMOTE_ADDR"],
+        //        //    email = datosUsuario.email,
+        //        //});
+        //        db.SaveChanges();
+        //        if (repositorioUsuario.validarUsuario(datosUsuario.usuario, md5(datosUsuario.clave)))
+        //        {
+        //            ViewBag.usuario = datosUsuario.usuario;
+        //            FormsAuthentication.SetAuthCookie(datosUsuario.usuario, true);
+        //            return RedirectToAction("Index", "Home");
+        //        }
+        //        else
+        //        {
+        //            return Content("No pudimos confirmar el inicio de seccion, intentalo mas tarde.");
+        //        }
+        //    }
+        //    return View();
 
         public ActionResult Salir()
         {
--- WikiWiki/WikiWiki/Controllers/Repositorios/RepositorioPublicacion.cs
+++ WikiWiki/WikiWiki/Controllers/Repositorios/RepositorioPublicacion.cs
@@ -47,10 +47,10 @@ namespace WikiWiki.Controllers.Repositorios
         // Mostrar publicacion segun su id
         public publicaciones getPublicacion(int id)
         {
-            var r = db.publicaciones.Join(db.Userios, p => p.usuario_id, u => u.usuario_id, (p, u) => new { publicacion_id = p.publicacion_id, fecha_publicacion = p.fecha_publicacion, titulo = p.titulo, fuente_de_informacion = p.fuente_de_informacion, categoria_id = p.categoria_id, usuarioP = u.usuario1, visitas = p.visitas })
-                .Join(db.categoria, p => p.categoria_id, c => c.id, (p, c) => new { publicacion_id = p.publicacion_id, fecha_publicacion = p.fecha_publicacion, titulo = p.titulo, fuente_de_informacion = p.fuente_de_informacion, usuarioP = p.usuarioP, categoriaP = c.categoria, visitas = p.visitas }).FirstOrDefault(u => u.publicacion_id == id);
+            var r = db.publicaciones.Join(db.Userios, p => p.usuario_id, u => u.usuario_id, (p, u) => new { publicacion_id = p.publicacion_id, informacion = p.informacion, fecha_publicacion = p.fecha_publicacion, titulo = p.titulo, fuente_de_informacion = p.fuente_de_informacion, categoria_id = p.categoria_id, usuarioP = u.usuario1, visitas = p.visitas })
+                .Join(db.categoria, p => p.categoria_id, c => c.id, (p, c) => new { publicacion_id = p.publicacion_id, informacion = p.informacion, fecha_publicacion = p.fecha_publicacion, titulo = p.titulo, fuente_de_informacion = p.fuente_de_informacion, usuarioP = p.usuarioP, categoriaP = c.categoria, visitas = p.visitas }).FirstOrDefault(u => u.publicacion_id == id);
 
-            var publicacion = new publicaciones { publicacion_id = r.publicacion_id, fecha_publicacion = r.fecha_publicacion, titulo = r.titulo, fuente_de_informacion = r.fuente_de_informacion, usuarioP = r.usuarioP, categoriaP = r.categoriaP, visitas = r.visitas };
+            var publicacion = new publicaciones { publicacion_id = r.publicacion_id, fecha_publicacion = r.fecha_publicacion, titulo = r.titulo, fuente_de_informacion = r.fuente_de_informacion, usuarioP = r.usuarioP, categoriaP = r.categoriaP, visitas = r.visitas, informacion = r.informacion };
 
             return publicacion;
         }
